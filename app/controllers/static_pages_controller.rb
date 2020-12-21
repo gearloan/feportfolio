3d86c9ca3420aa545eca041a9ec1a5f1cffa7ce4
gearloan <johnsfuller@gmail.com>
2020-12-21 15:47:29 -0500

    roughed in layout, css wip

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  def gallery
+  end
+  def resume
+  end
+  def code
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   get 'static_pages/index'
+  get 'static_pages/code'
+  get 'static_pages/resume'
+  get 'static_pages/gallery'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
