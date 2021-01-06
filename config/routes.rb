31c149368c585e25d8b9d10642c8ea431d8d65a9
gearloan <johnsfuller@gmail.com>
2021-01-06 13:14:37 -0500

    updated resume, rearranged page order

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#index'
+  root 'static_pages#gallery'
 
   get 'index', to: 'static_pages#index'
-  get 'static_pages/code'
+  get 'code', to: 'static_pages#code'
   get 'resume', to: 'static_pages#resume'
   get 'gallery', to:'static_pages#gallery'
   get 'personal', to: 'static_pages#personal'
