63a8622c707c5ccdad75ccde84c3563bbdcd16cd
gearloan <johnsfuller@gmail.com>
2021-01-07 15:18:39 -0500

    fixed nav flicker

--- app/javascript/packs/scrollNav.js
+++ app/javascript/packs/scrollNav.js
@@ -4,27 +4,29 @@ $(document).ready(function(){
   //down
   $('.project').waypoint(function(direction) {
   if (direction === 'down') {
-    $('.subnav a').removeClass('disable-click');
+    $('.subnav a').removeClass('active');
     // form the selector dynamically.
     // "this" keyword refers waypoint object and the element is located at "this.element"
     // using "this.element.id", get the nav anchor you want to target
     // example: "nav a[href='#landing']"
     var selector = ".subnav li a[href='#" + this.element.id + "']";
-    $(selector).addClass('disable-click');
+    $(selector).addClass('active');
     console.log(selector);
   }
   }, {
-  offset: '5%'
+  offset: '50%',
+  triggerOnce: true,
 });
 //up
   $('.project').waypoint(function(direction) {
     if (direction === 'up') {
-      $('.subnav a').removeClass('disable-click');
+      $('.subnav a').removeClass('active');
       var selector = ".subnav li a[href='#" + this.element.id + "']";
-      $(selector).addClass('disable-click');
+      $(selector).addClass('active');
     }
   }, {
-    offset: '5%'
+    offset: '-100%',
+    triggerOnce: true,
 });
 
 /////////////////// scrollto ////////////
