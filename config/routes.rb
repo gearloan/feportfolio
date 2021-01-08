953adea1d7acdfd879f5a193e83b1349556b1d95
gearloan <johnsfuller@gmail.com>
2021-01-08 17:31:28 -0500

    added two imssing logos

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'code', to: 'static_pages#code'
   get 'resume', to: 'static_pages#resume'
   get 'gallery', to:'static_pages#gallery'
-  get 'personal', to: 'static_pages#personal'
+  get 'about', to: 'static_pages#personal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
